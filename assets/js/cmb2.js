67425b5b1ba0b4b054fccd448c03475ecfe3524b
Cristian Frumusanu <cristian.frumusanu@pixelgrade.com>
2016-06-07 11:35:56 +0300

    Font uploads field style

--- assets/js/cmb2.js
+++ assets/js/cmb2.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function ($) {
 
-    //no nothing right now
+	$('#fonto_font_files-status').bind('DOMSubtreeModified', function() {
+		if( $(this).is(':empty') ) {
+			$(this).parent().removeClass('is--populated');
+		} else {
+			$(this).parent().addClass('is--populated');
+		}
+	});
 
+	$('#fonto_font_files-status').on('click', function() {
+		$('.cmb2-id-fonto-font-files .cmb2-upload-button').trigger('click');
+	});
+
+	$('#fonto_font_files-status').after('<i class="edit-fonts-icon  icon  dashicons  dashicons-images-alt2"></i>');
 });
