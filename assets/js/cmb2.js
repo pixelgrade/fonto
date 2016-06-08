0077e8faf43aa612e36bfb50bc54262cf73ebd76
Cristian Frumusanu <cristian.frumusanu@pixelgrade.com>
2016-06-08 11:50:59 +0300

    Style enhancements

--- assets/js/cmb2.js
+++ assets/js/cmb2.js
@@ -1,18 +1,22 @@
 jQuery(document).ready(function ($) {
 
-	$('#fonto_font_files-status').bind('DOMSubtreeModified', function() {
-		if( $(this).is(':empty') ) {
-			$(this).parent().removeClass('is--populated');
+	$( '#fonto_font_files-status' ).bind( 'DOMSubtreeModified', function() {
+		if( $( this ).is( ':empty' ) ) {
+			$( this ).parent().removeClass( 'is--populated' );
 		} else {
-			$(this).parent().addClass('is--populated');
+			$( this ).parent().addClass( 'is--populated' );
 		}
 	});
 
-	$('#fonto_font_files-status').on('click', function() {
-		$('.cmb2-id-fonto-font-files .cmb2-upload-button').trigger('click');
+	$( window ).load( function() {
+		$( 'body' ).addClass( 'is--loaded' );
 	});
 
-	$('#fonto_font_files-status').after('<i class="edit-fonts-icon  icon  dashicons  dashicons-images-alt2"></i>');
+	//$( '#fonto_font_files-status' ).on( 'click', function() {
+	//	$( '.cmb2-id-fonto-font-files .cmb2-upload-button' ).trigger( 'click' );
+	//});
+
+	//( '#fonto_font_files-status' ).after( '<i class="edit-fonts-icon  icon  dashicons  dashicons-images-alt2"></i>');
 
 	//Require post title when adding/editing Fonts
 	//inspired by http://kellenmace.com/require-post-title-for-custom-post-type/
