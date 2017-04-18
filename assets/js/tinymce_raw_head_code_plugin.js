5c078fa578b67b123f306c6358f0f05ad31de1ab
andreilupu <andrei.lupu@pixelgrade.com>
2017-04-18 11:13:49 +0300

    fix #5

--- assets/js/tinymce_raw_head_code_plugin.js
+++ assets/js/tinymce_raw_head_code_plugin.js
@@ -1,14 +1,16 @@
-(function( tinymce ) {
-    tinymce.PluginManager.add('raw_head_code', function( editor ) {
-        editor.on('preinit', function ( args) {
-            // find the iframe which relates to this instance of the editor
-            var iframe = jQuery("#" + args.target.id + "_ifr");
+(function (tinymce) {
+	tinymce.PluginManager.add('raw_head_code', function (editor) {
+		editor.on('preinit', function (args) {
+			// find the iframe which relates to this instance of the editor
+			var iframe = jQuery("#" + args.target.id + "_ifr");
 
-            // get the head element of the iframe's document and inject the code
-            jQuery(iframe[0].contentWindow.document)
-                .children('html')
-                .children('head')
-                .append( tinymce_raw_head_code.code ); //this one is available globally through script localization; see class-fonto-output.php@237
-        });
-    });
-}( window.tinymce ));
+			if ( typeof tinymce_raw_head_code !== "undefined" ) {
+				// get the head element of the iframe's document and inject the code
+				jQuery(iframe[0].contentWindow.document)
+					.children('html')
+					.children('head')
+					.append(tinymce_raw_head_code.code); //this one is available globally through script localization; see class-fonto-output.php@237
+			}
+		});
+	});
+}(window.tinymce));
