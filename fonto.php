a233946c553baf769f62314c5a8b548d2d3790f1
George Olaru <george@pixelgrade.com>
2024-10-16 11:15:48 +0300

    Bump version to 1.2.2

--- fonto.php
+++ fonto.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Plugin Name: Fonto - Custom Web Fonts Manager
- * Version: 1.2.1
- * Plugin URI: https://pixelgrade.com
+ * Version: 1.2.2
+ * Plugin URI: https://wordpress.org/plugins/fonto
  * Description: Use your premium web fonts directly in the Editor or with the Customify and Style Manager plugins. Works with Typekit, MyFonts, Fonts.com, self-hosted fonts, and others.
  * Author: Pixelgrade
  * Author URI: https://pixelgrade.com
  * Requires at least: 4.9.9
- * Tested up to: 5.9.2
+ * Tested up to: 6.6.2
+ * License: GPL v2.0 (or later)
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: fonto
  * Domain Path: /languages/
  */
@@ -25,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function fonto() {
 
 	require_once( 'includes/class-fonto.php' );
-	$instance = Fonto::instance( __FILE__, '1.2.1' );
+	$instance = Fonto::instance( __FILE__, '1.2.2' );
 
 	return $instance;
 }
